1bacd3fb28d23b3dd76eef724b0a13552bd8311a
wusishuang <wusishuang@foxmail.com>
2021-01-29 16:27:22 +0800

    elementUI组件国际化

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import './plugins'
 import '@/layouts/export'
 import VueI18n from 'vue-i18n'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import localeEn from '../node_modules/element-ui/lib/locale/lang/en'
+import localeJa from '../node_modules/element-ui/lib/locale/lang/ja'
+import localeCn from '../node_modules/element-ui/lib/locale/lang/zh-CN'
+
 let lang = myExtension.getlanguage()
 Vue.use(VueI18n)
 const i18n = new VueI18n({
@@ -17,6 +23,14 @@ const i18n = new VueI18n({
   },
 })
 Vue.prototype.$lang = lang
+let locale = localeCn
+if (lang == 'Jan_JPN') {
+  locale = localeJa
+} else if (lang == 'en_US') {
+  locale = localeEn
+}
+
+Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
