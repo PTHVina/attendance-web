16c19f8454fc27c9feb83ba362493e7c091cfd2f
benny <shenbinsc@gmail.com>
2022-03-18 15:19:43 +0800

    关闭回车符警告

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,12 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/no-v-html': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
   parserOptions: {
     parser: 'babel-eslint',
